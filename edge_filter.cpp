164c80edc73d1d4f66219bfc0f6db476a7a5f610
ururobo <lainfuthu@yahoo.co.jp>
2016-02-04 23:19:41 +0900

    ver2.0

--- edge_filter.cpp
+++ edge_filter.cpp
@@ -1,7 +1,36 @@
 #include "edge_filter.h"
 
+void Edge_Filter_rgb(cv::Mat& src_img, cv::Mat& dst_img) {
+	cv::Mat channel[3];
+	cv::split(src_img, channel);
+
+	cv::Mat filtered_img[3];
+	int i;
+	for (i = 0; i < 3; ++i) {
+		filtered_img[i] = cv::Mat(channel[i].rows, channel[i].cols, CV_8UC1);
+		Edge_Filter_rgb_Processing(channel[i], filtered_img[i]);
+	}
+
+	dst_img = cv::max(max(filtered_img[0], filtered_img[1]), filtered_img[2]);
+}
+
+void Edge_Filter_rgb_Processing(cv::Mat& src_img, cv::Mat& dst_img) {
+	cv::Mat height_img = cv::Mat(src_img.rows, src_img.cols, CV_8UC1);
+	cv::Mat width_img = cv::Mat(src_img.rows, src_img.cols, CV_8UC1);
+	cv::Mat total_img = cv::Mat(src_img.rows, src_img.cols, CV_8UC1);
+
+	int array_y[3][3] = { { 1,2,1 },{ 0,0,0 },{ -1,-2,-1 } };
+	int array_x[3][3] = { { 1,0,-1 },{ 2,0,-2 },{ 1,0,-1 } };
+
+	Filter(src_img, height_img, array_y);
+	Filter(src_img, width_img, array_x);
+	Total(height_img, width_img, total_img);
+
+	total_img.copyTo(dst_img);
+}
+
 void Edge_Filter(cv::Mat& src_img, cv::Mat& dst_img) {
-	cv::Mat gray_img = cv::Mat(src_img.rows, src_img.cols, CV_8UC1);
+	cv::Mat gray_img = cv::Mat(src_img.rows, src_img.cols, CV_8U);
 	cvtColor(src_img, gray_img, CV_BGR2GRAY);
 
 	cv::Mat height_img = cv::Mat(gray_img.rows, gray_img.cols, CV_8UC1);
@@ -18,7 +47,7 @@ void Edge_Filter(cv::Mat& src_img, cv::Mat& dst_img) {
 	total_img.copyTo(dst_img);
 }
 
-void Filter(cv::Mat& src, cv::Mat& dst, int kernel[3][3]) {
+void Filter(cv::Mat& src, cv::Mat& dst, const int kernel[3][3]) {
 	int sum = 0;
 	for (int y = 1; y < src.rows - 1; ++y) {
 		for (int x = 1; x < src.cols - 1; ++x) {
--- edge_filter.h
+++ edge_filter.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <opencv2\opencv.hpp>
 
+void Edge_Filter_rgb(cv::Mat& src_img, cv::Mat& dst_img);
+void Edge_Filter_rgb_Processing(cv::Mat& src_img, cv::Mat& dst_img);
 void Edge_Filter(cv::Mat& src_img, cv::Mat& dst_img);
-void Filter(cv::Mat& src, cv::Mat& dst, int kernel[3][3]);
+void Filter(cv::Mat& src, cv::Mat& dst, const int kernel[3][3]);
 void Total(cv::Mat& src1, cv::Mat& src2, cv::Mat& dst);
